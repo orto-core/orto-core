cf98e208f0138904bc10a4da23177679349fc5db
Nathan Kulewoshie <kulewoshienathan@gmail.com>
2024-08-19 20:01:37 +0000

    chore: added a command in dockerfiles to grant application access to read and write the .env in the image

--- deploy-scripts/after-install.sh
+++ deploy-scripts/after-install.sh
@@ -11,5 +11,5 @@ if [ "$useKubernetes" = true ]; then
 	done
 else
 	cd "/home/projects/orto-micro-service"
-	docker compose -f "docker-compose.yml" up --remove-orphans -d
+	sudo docker compose -f "docker-compose.yml" up --remove-orphans -d
 fi
